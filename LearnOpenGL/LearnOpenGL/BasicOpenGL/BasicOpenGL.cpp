4ed9bbd4ee8eff4689eb4a8b249a4890095bc3a7
wzl108 <wzlmakegames@gmail.com>
2020-10-16 18:10:58 +0800

    learn c++

--- LearnOpenGL/LearnOpenGL/BasicOpenGL/BasicOpenGL.cpp
+++ LearnOpenGL/LearnOpenGL/BasicOpenGL/BasicOpenGL.cpp
@@ -1,11 +1,28 @@
-﻿// BasicOpenGL.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include <array>
 #include <iostream>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+
+namespace a::b::c
+{
+    inline constexpr std::string_view str{ "hello" };
+}
+
+template <class... T>
+std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args)
+{
+    return { sizeof...(T), (args + ...) };
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto [iNumbers, iSum] { sum(1, 2, 3) };
+    std::cout << a::b::c::str << ' ' << iNumbers << ' ' << iSum << '\n';
+
+    std::array arr{ 1, 2, 3 };
+
+    std::cout << std::size(arr) << '\n';
+
+    return 0;
 }
-
-
